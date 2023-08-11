eec0af5baa4dac717164d2359bb1e3add1485a88
Lotboks <110311548+Lotboks@users.noreply.github.com>
2023-08-11 11:53:16 +0200

    class 3 domasna

---
+++ Class03_Homework/Class03_WebApi_Homework/Class03_WebApi_Homework/Controllers/BooksController.cs
@@ -0,0 +1,94 @@
+﻿using Class03_WebApi_Homework.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Class03_WebApi_Homework.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Book>> Get()
+        {
+            return Ok(StaticDb.Books);
+        }
+
+        [HttpGet("queryString")]
+        public ActionResult<Book> GetByQueryString(int? index)
+        {
+            try
+            {
+                if (index == null)
+                {
+                    return BadRequest("Index is required.");
+                }
+                if (index < 0)
+                {
+                    return BadRequest("Index can only be a positive value");
+                }
+                if (index >= StaticDb.Books.Count)
+                {
+                    return NotFound($"There is no resource with an index of {index}");
+                }
+                return Ok(StaticDb.Books[index.Value]);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error occured");
+            }
+        }
+
+
+        [HttpGet("filter")]
+        public ActionResult<List<Book>> FilterBooks(string? author, string? title)
+        {
+            try
+            {
+                if(string.IsNullOrEmpty(author) && string.IsNullOrEmpty(title))
+                {
+                    return BadRequest("Insert at least one query paramater");
+                }
+                if (!string.IsNullOrEmpty(author))
+                {
+                    return Ok(StaticDb.Books.Where(x => x.Author.ToLower() == author.ToLower()).ToList());
+                }
+                if(!string.IsNullOrEmpty(title))
+                {
+                    return Ok(StaticDb.Books.Where(x => x.Title.ToLower() == title.ToLower()).ToList());
+                }
+                if (StaticDb.Books.Count == 0)
+                {
+                    return NotFound($"Book with name and/or author was not found.");
+                }
+                return Ok(StaticDb.Books.Where(x => x.Author.ToLower() == author.ToLower() && x.Title.ToLower() == title.ToLower()).ToList());
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "A server error occure.");
+            }
+        }
+
+        [HttpPost]
+        public IActionResult AddBook([FromBody] Book book)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(book.Author))
+                {
+                    return BadRequest("Please enter author");
+                }
+                if (string.IsNullOrEmpty(book.Title))
+                {
+                    return BadRequest("Please enter title");
+                }
+                StaticDb.Books.Add(book);
+                return StatusCode(StatusCodes.Status201Created);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "A server error occured.");
+            }
+        }
+    }
+}
---
+++ Class03_Homework/Class03_WebApi_Homework/Class03_WebApi_Homework/StaticDb.cs
@@ -0,0 +1,34 @@
+﻿using Class03_WebApi_Homework.Models;
+
+namespace Class03_WebApi_Homework
+{
+    public static class StaticDb
+    {
+        public static List<Book> Books = new List<Book>()
+        {
+            new Book()
+            {
+                Author = "J. Robert Oppenheimer",
+                Title = "Uncommon Sense",
+            },
+
+            new Book()
+            {
+                Author = "Brian Griffin",
+                Title = "Faster Than The Speed Of Love",
+            },
+
+            new Book()
+            {
+                Author = "William Shakespeare",
+                Title = "Hamlet",
+            },
+
+            new Book()
+            {
+                Author = "Adolf Hitler",
+                Title = "Mein Kampf",
+            }
+        };
+    }
+}
